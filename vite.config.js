1113bfd1e1492907daa30415b918e1c6149e6ee6
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-01-20 23:26:12 +0700

    serve

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        outDir: path.join(__dirname, "_static")
+      }
 });

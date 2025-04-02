237e3e7762f8a8d89cedad141511cf36e1a623e7
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-04-02 12:26:39 +0700

    chnage command

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true, // ✅ เปิดให้ใช้จากมือถือ
-        strictPort: true,
-        port: 5173, // ✅ กำหนดพอร์ต
-        cors: true,
-      },
+    plugins: [react()]
 });

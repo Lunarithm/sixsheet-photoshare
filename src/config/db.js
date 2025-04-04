22e9343cf36149891d43cd82c778985bdda3a6b4
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-04-04 13:24:10 +0700

    edit string

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize('postgresql://doadmin:AVNS_yqbAKywnht18sdKZ3BB@sixsheet-postgres-do-user-10822787-0.b.db.ondigitalocean.com:25060/photoshare?sslmode=no-verify');
+const sequelize = new Sequelize('postgresql://doadmin:AVNS_XopmdTy8f8fFg-4b2m5@sixsheet-photoshare-db-do-user-10822787-0.d.db.ondigitalocean.com:25060/photoshare?sslmode=no-verify');
 
 export default sequelize;

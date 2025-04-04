9956b0fb283b6c0ab94295a701204f1c709167c8
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-04-04 12:53:28 +0700

    change db schema

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize('postgresql://doadmin:AVNS_yqbAKywnht18sdKZ3BB@sixsheet-postgres-do-user-10822787-0.b.db.ondigitalocean.com:25060/sixsheet?sslmode=no-verify');
+const sequelize = new Sequelize('postgresql://doadmin:AVNS_yqbAKywnht18sdKZ3BB@sixsheet-postgres-do-user-10822787-0.b.db.ondigitalocean.com:25060/photoshare?sslmode=no-verify');
 
 export default sequelize;

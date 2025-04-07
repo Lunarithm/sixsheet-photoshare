0f76c35cd06fb4dd2869d1b791a87002271e989a
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-04-07 23:58:09 +0700

    remove db credential change package.json

--- src/config/db.js
+++
@@ -1,5 +0,0 @@
-import { Sequelize } from 'sequelize'
-
-const sequelize = new Sequelize('postgresql://doadmin:AVNS_XopmdTy8f8fFg-4b2m5@sixsheet-photoshare-db-do-user-10822787-0.d.db.ondigitalocean.com:25060/photoshare?sslmode=no-verify');
-
-export default sequelize;

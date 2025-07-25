a300e272f957e770769b87218b87b650e01b96c4
Kritchapon Choktananan <kritchapon.c@gmail.com>
2025-07-25 22:39:13 +0700

    chagen to mp4

--- src/App.jsx
+++ src/App.jsx
@@ -469,7 +469,7 @@ function App() {
                         if (image) {
                           saveAs(imgFile, `image_${shortUUID}.jpg`);
                         } else if (vdo) {
-                          saveAs(vdoFile, `video_${shortUUID}.jpg`);
+                          saveAs(vdoFile, `video_${shortUUID}.mp4`);
                         }
                       }}
                     >
